cc9c35bdaae167f0b863c4ee89c7aafa1d532649
Abhijit <abhijit.nandy@gmail.com>
2025-01-11 02:15:51 +0530

    Initial Commit.

---
+++ src/API/RAppsAPI/RAppsAPI/Data/RDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using RAppsAPI.Entities;
+
+
+namespace RAppsAPI.Data
+{
+    public class RDBContext: IdentityDbContext<RAppUser>
+    {
+        public RDBContext(DbContextOptions<RDBContext> options) : base(options) 
+        { 
+            
+        }
+    }
+}
---
+++ src/API/RAppsAPI/RAppsAPI/Entities/RAppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RAppsAPI.Entities
+{
+    public class RAppUser : IdentityUser
+    {
+        public string Username {  get; set; }
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }        
+        public DateTime CreatedOn { get; set; }
+        public int RStatus { get; set; }
+    }
+}
