edf7270b5151b493b3fedc763d36ab7886db5b86
Abhijit <abhijit.nandy@gmail.com>
2025-03-14 23:23:37 +0530

    Correction to class.

--- src/API/RAppsAPI/RAppsAPI/Models/MPMRangeInfoResult.cs
+++ src/API/RAppsAPI/RAppsAPI/Models/MPMRangeInfoResult.cs
@@ -1,16 +1,57 @@
 ﻿namespace RAppsAPI.Models.MPM
 {
-    internal class MPMRangeInfoResult
+    public class MPMRangeInfoResult
     {
-        public int SeriesCount { get; set; }
-        public int CellId { get; set; }
-        public int RowNum { get; set; }
-        public int ColNum { get; set; }
-        public string Value { get; set; } = string.Empty;
-        public string Formula { get; set; } = string.Empty;
-        public string Format { get; set; } = string.Empty;
-        public string Style { get; set; } = string.Empty;
-        public string Comment { get; set; } = string.Empty;
-       
+        public int SeriesCount { get; }
+        public int CellId { get; }
+        public int RowNum { get; }
+        public int ColNum { get; }
+        public string Value { get; }
+        public string Formula { get; }
+        public string Format { get; }
+        public string Style { get; }
+        public string Comment { get; }
+
+        public MPMRangeInfoResult(int seriesCount, int cellId, int rowNum, int colNum, string value, string formula, string format, string style, string comment)
+        {
+            SeriesCount = seriesCount;
+            CellId = cellId;
+            RowNum = rowNum;
+            ColNum = colNum;
+            Value = value;
+            Formula = formula;
+            Format = format;
+            Style = style;
+            Comment = comment;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is MPMRangeInfoResult other &&
+                   SeriesCount == other.SeriesCount &&
+                   CellId == other.CellId &&
+                   RowNum == other.RowNum &&
+                   ColNum == other.ColNum &&
+                   Value == other.Value &&
+                   Formula == other.Formula &&
+                   Format == other.Format &&
+                   Style == other.Style &&
+                   Comment == other.Comment;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(SeriesCount);
+            hash.Add(CellId);
+            hash.Add(RowNum);
+            hash.Add(ColNum);
+            hash.Add(Value);
+            hash.Add(Formula);
+            hash.Add(Format);
+            hash.Add(Style);
+            hash.Add(Comment);
+            return hash.ToHashCode();
+        }
     }
 }
